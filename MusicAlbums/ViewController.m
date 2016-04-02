fb989c27720b374085ae60adfdc0653b77d11d52
ycpjobs <1175585627@qq.com>
2016-04-02 20:32:54 +0800

    Decorator Design Pattern ,使用代理模式

--- MusicAlbums/ViewController.m
+++ MusicAlbums/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#import "LibraryAPI.h"
+#import "Album+TableRepresentation.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>{
+    UITableView *dataTable;
+    NSArray *allAlbums;
+    NSDictionary *currentAlbumData;
+    int currentAlbumIndex;
+}
 
 @end
 
@@ -16,12 +23,54 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor =  [UIColor colorWithRed:0.76f green:0.81f blue:0.87f alpha:1];
+    currentAlbumIndex = 0;
+    
+    allAlbums = [[LibraryAPI sharedInstance] getAlbums];
+    
+    //the uitableview that presents the album data
+    dataTable = [[UITableView alloc] initWithFrame:CGRectMake(0, 120, self.view.frame.size.width, self.view.frame.size.height-120) style:UITableViewStyleGrouped];
+    dataTable.delegate =self;
+    dataTable.dataSource = self;
+    dataTable.backgroundView = nil;
+    [self.view addSubview:dataTable];
+    
+    [self showDataForAlbumAtIndex:currentAlbumIndex];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)showDataForAlbumAtIndex:(int)albumIndex
+{
+    if (albumIndex < allAlbums.count) {
+        // fetch the album
+        Album *album = allAlbums[albumIndex];
+        // save the albums data to present it later in the tableview
+        currentAlbumData = [album tr_tableRepresentation];
+    }else
+    {
+        currentAlbumData = nil;
+    }
+    
+    // we have the data we need, let's refresh our tableview
+    [dataTable reloadData];
 }
 
+#pragma mark - UITableViewDataSource
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [currentAlbumData[@"titles"] count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
+    }
+    
+    cell.textLabel.text = currentAlbumData[@"titles"][indexPath.row];
+    cell.detailTextLabel.text = currentAlbumData[@"values"][indexPath.row];
+    
+    return cell;
+}
 @end
